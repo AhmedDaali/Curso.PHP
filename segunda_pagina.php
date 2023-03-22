366512ff33940601950d9c7dc8066334a701a5f2
AhmedDaali <ahmeddaali2@gmail.com>
2023-03-22 13:36:03 +0100

    Prueba rama ahmed

--- segunda_pagina.php
+++ segunda_pagina.php
@@ -14,10 +14,10 @@
 
     print "El nombre del usuario es: " .  $nombre  . "<br>";
     print "El nombre del usuario es:  $nombre<br>";//Escribe el valor de la  variable
-    print 'El nombre del usuario es:  $nombre <br>';//escribe el literal
+    print 'El nombre del usuario es:  $nombre <br>';//escribe el literal y no la variable almacenada dentro
     
     echo $nombre, $edad;//adimete varias variables sin concatenar, también es mas comun y rápido
-    echo "<br>El nombre del usuario es:  $nombre y tiene $edad años";
+    echo "<br>El nombre del usuario es:  $nombre y     tiene $edad años";
     ?>
     
 </body>
