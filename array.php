003e51cffe7ceb422ace35ca79adbc3313701317
AhmedDaali <ahmeddaali2@gmail.com>
2023-03-27 17:34:21 +0200

    Array 2 y documentación

--- array.php
+++ array.php
@@ -8,18 +8,54 @@
 </head>
 <body>
     <?php
+        // Declaración de un array llamado $semana
+    // El índice 0 se asignará automáticamente al primer elemento, luego se le asignarán índices consecutivos
     $semana [] ="Lunes";//Sino ponemos el índice, php lo pone automáticamente.
     $semana [] ="Martes";
     $semana [2] ="Miercoles";
     $semana [3] ="Jueves";
 
-    $semana2 = array("Lunes","Martes","Miercoles","Jueves");
+    // Declaración de un array llamado $semana2
+    // Se le asignan todos los elementos del array al momento de declararlo
+    $semana2 = array("Lunes","Martes","Miercoles","Jueves");//Meter todos los datos de una vez.
+
+    // Declaración de un array asociativo llamado $semana3
+    // Los elementos del array se identifican con una clave, en este caso "dia1", "dia2", "dia3" y "dia4"
+    $semana3 = array("dia1"=>"Lunes", "dia2"=>"Martes","dia3"=>"Miercoles","dia4"=>"Jueves");//A modo de diccionário.
 
     echo $semana[0] . "<br/>";
     echo $semana2[0] . "<br/>";
-    echo $semana [3] . "<br/>";
-    echo $semana[2] . "<br/>";
+    echo $semana3["dia1"] . "<br/>";
+    echo"====================<br/>";
 
+    echo $semana [3] . "<br/>";
+    echo $semana2[2] . "<br/>";
+    echo"====================<br/>";
+
+    
+    echo $semana3["dia2"] . "<br/>";
+    echo $semana3["dia3"] . "<br/>";
+    echo $semana3["dia4"] . "<br/>";
+    echo"====================<br/>";
+
+    foreach ($semana3 as $clave =>$valor){//Recorrer el diccionário. 
+
+    echo $clave . ": " . $valor . "<br/>";
+    }
+    echo"====================<br/>";
+
+    for($i = 0; $i <count ($semana2 ); $i++){
+        sort ($semana2);//Ordena los valores de un array alfabéticamente
+        echo $i . ": ". $semana2[$i] . "<br/>";
+    }
+    echo"====================<br/>";
+    //Añadimos elemntos al array asociativo.
+    $semana3["dia5"] = "Viernes";
+    foreach ($semana3 as $clave =>$valor){//Recorrer el diccionário. 
+        
+        echo $clave . ": " . $valor . "<br/>";
+        }
+    
     ?>
 </body>
 </html>
--- coche.php
+++ coche.php
@@ -1,58 +1,127 @@
 <?php
+/**
+ * Clase Coche
+ * 
+ * Esta clase representa un coche con propiedades como el número de ruedas, la marca, el color y el motor.
+ * También tiene métodos como arrancar, girar y frenar.
+ */
 class Coche  {
-    /** @var int $ruedas */
+    /** @var int $ruedas El número de ruedas del coche */
     private int $ruedas;
+    /** @var string $marca La marca del coche */
     private string $marca;
+    /** @var string $color El color del coche */
     private string $color;
+    /** @var int $motor El tamaño del motor en cc */
     private int $motor;
     
 
-
-    public function __construct($marca_coche, $color_coche){//Constructor
+    /**
+     * Constructor
+     * 
+     * Crea una instancia de la clase Coche con la marca y el color especificados.
+     *
+     * @param string $marca_coche La marca del coche
+     * @param string $color_coche El color del coche
+     */
+    public function __construct($marca_coche, $color_coche){
         $this->marca = $marca_coche;
         $this->ruedas = 4;
-        $this-> color = $color_coche;
+        $this->color = $color_coche;
         $this->motor = 1600;
     }
 
-    function get_ruedas (){
-        return $this->ruedas;
-    }
-    function get_marca (){
-        return $this->marca;
-    }
-    function get_color(){
-        return $this->color;
-    }
-    function get_motor (){
-        return $this->motor;
-    }
-    function set_marca($marca_coche){
-        $this->marca = $marca_coche;
-    }
-    function set_ruedas($ruedas_coche){
-        $this->ruedas = $ruedas_coche;
-    }
+    /**
+    * Método para obtener la cantidad de ruedas del coche.
+    * 
+    * @return int Cantidad de ruedas del coche.
+    */
+   function get_ruedas (){
+       return $this->ruedas;
+   }
 
-    function set_color($color_coche){
-        $this->color = $color_coche;
-    }
+   /**
+    * Método para obtener la marca del coche.
+    * 
+    * @return string Marca del coche.
+    */
+   function get_marca (){
+       return $this->marca;
+   }
 
-    function set_motor($motor_coche){
-        $this->motor = $motor_coche;
-    }
+   /**
+    * Método para obtener el color del coche.
+    * 
+    * @return string Color del coche.
+    */
+   function get_color(){
+       return $this->color;
+   }
 
+   /**
+    * Método para obtener la potencia del motor del coche.
+    * 
+    * @return int Potencia del motor del coche.
+    */
+   function get_motor (){
+       return $this->motor;
+   }
 
+   /**
+    * Método para establecer la marca del coche.
+    * 
+    * @param string $marca_coche Nueva marca del coche.
+    */
+   function set_marca($marca_coche){
+       $this->marca = $marca_coche;
+   }
 
-    function arrancar(){
-        echo"Coche arrancando<br/>";
-    }
-    function girar(){
-        echo"Coche girando<br/>";
-    }
+   /**
+    * Método para establecer la cantidad de ruedas del coche.
+    * 
+    * @param int $ruedas_coche Nueva cantidad de ruedas del coche.
+    */
+   function set_ruedas($ruedas_coche){
+       $this->ruedas = $ruedas_coche;
+   }
+
+   /**
+    * Método para establecer el color del coche.
+    * 
+    * @param string $color_coche Nuevo color del coche.
+    */
+   function set_color($color_coche){
+       $this->color = $color_coche;
+   }
+
+   /**
+    * Método para establecer la potencia del motor del coche.
+    * 
+    * @param int $motor_coche Nueva potencia del motor del coche.
+    */
+   function set_motor($motor_coche){
+       $this->motor = $motor_coche;
+   }
+
+   /**
+    * Método para arrancar el coche.
+    */
+   function arrancar(){
+       echo"Coche arrancando<br/>";
+   }
+
+   /**
+    * Método para girar el coche.
+    */
+   function girar(){
+       echo"Coche girando<br/>";
+   }
+
+   /**
+    * Método para frenar el coche.
+    */
     function frenar(){
         echo"Coche frenando<br/>";
     }
-
 }
 ?>
